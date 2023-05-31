97b3cd31ef322b38de08b300b16c28cf16e7f2d3
Sinpo <Sinpo96@163.com>
2023-06-01 00:39:48 +0800

    refactor: Separating Webpack 4 and Webpack 5.

---
+++ examples/webpack5-use-dev-sync-injector/src/index.js
@@ -0,0 +1,5 @@
+const testFun = require('simplify-yalc-dev');
+
+testFun();
+
+console.log('4');
---
+++ examples/webpack5-use-dev-sync-injector/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { webpackPlugin: DevSyncInjector } = require('dev-sync-injector');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {},
+  plugins: [new HtmlWebpackPlugin(), new DevSyncInjector(), new webpack.HotModuleReplacementPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
